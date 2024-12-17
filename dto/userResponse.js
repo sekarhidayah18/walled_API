ea8aa850dbae86d9631e8c988200350710fd6d76
Adityo <adigiz@yahoo.com>
2024-12-17 11:58:40 +0700

    fix: missing avatar url

--- dto/userResponse.js
+++ dto/userResponse.js
@@ -4,6 +4,7 @@ class UserResponse {
     this.username = user.username;
     this.fullname = user.fullname;
     this.email = user.email;
+    this.avatar_url = user.avatar_url;
     if (user.wallet) {
       this.wallet = {
         account_number: user.wallet.account_number,
